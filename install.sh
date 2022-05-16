09e6eb8c318869dd619aed0501977bf416e2c2e0
leenperjasknegt <leenknegtjasper@gmail.com>
2022-05-16 16:27:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -53,7 +53,7 @@ sed -i "9c\ExecStart=/usr/bin/python3 -m IntegraPy.demo $varintegraip" /etc/syst
 fi
 echo
 echo "###################################################################"
-echo "Camect URL: (visit https://local.home.camect.com and paste the link in here; example: https://ebbabdd9a.l.home.camect.com/"
+echo "Camect URL: (visit https://local.home.camect.com and paste the link in here; for example: https://ebbabdd9a.l.home.camect.com/)"
 read varcamectip
 echo "Wachtwoord Camect (prefix emailadres):"
 read varcamectpassword
