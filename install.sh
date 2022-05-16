dfa1e3bcd7d65806f8667710174e0126aff6d09d
leenperjasknegt <leenknegtjasper@gmail.com>
2022-05-16 12:52:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@
 # API for connecting Camect with Satel Integra ETHM module.       #                                                                                                                                                                                     
 # Author: JL                                                      #                            
 ###################################################################
-exec > /log.txt 2>&1
-# Everything below will go to the file 'log.out':
+
 
 echo "Did you accept the terms at https://camect.local and enabled Integration in Satel ETHM settings?"
 select yn in "Yes" "No"; do
@@ -19,29 +18,29 @@ echo "###################################################################"
 echo "Installing Python3"
 echo "###################################################################"
 echo
-sudo apt update > /dev/null 2>&1
-sudo apt-get -y install python3-pip > /dev/null 2>&1
+sudo apt update > file.log 2>&1
+sudo apt-get -y install python3-pip > file.log 2>&1
 echo
 echo "###################################################################"
 echo "Installing IntegraPy"
 echo "###################################################################"
 echo
-sudo pip3 install IntegraPy > /dev/null 2>&1
+sudo pip3 install IntegraPy > file.log 2>&1
 echo
 echo "###################################################################"
 echo "Installing Wget"
 echo "###################################################################"
 echo
-sudo apt install wget > /dev/null 2>&1
+sudo apt install wget > file.log 2>&1
 echo
 echo "###################################################################"
 echo "Downloading & copy files"
 echo "###################################################################"
 echo
-wget https://raw.githubusercontent.com/leenperjasknegt/camect-satel/main/camect.service > /dev/null 2>&1
-wget https://raw.githubusercontent.com/leenperjasknegt/camect-satel/main/demo.py > /dev/null 2>&1
-sudo mv demo.py /usr/local/lib/python3.8/dist-packages/IntegraPy/demo.py > /dev/null 2>&1
-sudo mv camect.service /etc/systemd/system/camect.service > /dev/null 2>&1
+wget https://raw.githubusercontent.com/leenperjasknegt/camect-satel/main/camect.service > file.log 2>&1
+wget https://raw.githubusercontent.com/leenperjasknegt/camect-satel/main/demo.py > file.log 2>&1
+sudo mv demo.py /usr/local/lib/python3.8/dist-packages/IntegraPy/demo.py > file.log 2>&1
+sudo mv camect.service /etc/systemd/system/camect.service > file.log 2>&1
 echo
 echo "###################################################################"
 echo "IP adress Satel Integra ETHM:"
@@ -70,18 +69,18 @@ echo "###################################################################"
 echo "Creating Camect Service"
 echo "###################################################################"
 echo
-sudo systemctl stop camect.service > /dev/null 2>&1
+sudo systemctl stop camect.service > file.log 2>&1
 sleep 2
-sudo systemctl daemon-reload > /dev/null 2>&1
+sudo systemctl daemon-reload > file.log 2>&1
 sleep 1
-sudo systemctl enable camect.service > /dev/null 2>&1
+sudo systemctl enable camect.service > file.log 2>&1
 echo
 echo "###################################################################"
 echo "Start Camect Service"
 echo "###################################################################"
 echo
 sleep 1
-sudo systemctl start camect.service > /dev/null 2>&1
+sudo systemctl start camect.service > file.log 2>&1
 echo
 sleep 2
 echo
