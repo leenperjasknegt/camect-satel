07be7e0f4e9c57d61833a6cf17e0f2b3c0953488
leenperjasknegt <leenknegtjasper@gmail.com>
2022-05-24 21:57:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -100,9 +100,10 @@ echo
 echo "###################################################################"
 read -p "Do you want to inverse the zone? From NC to NO? (y/n)" CONT
 if [ "$CONT" = "y" ]; then
+  echo "NC"
   sudo sed -i "43c\     if integrapartition in armed_partitions and not integrazone in violated_zones :" /usr/local/lib/python3.8/dist-packages/IntegraPy/demo.py
 else
-  echo "booo";
+  echo "NO";
 fi
 
 echo
