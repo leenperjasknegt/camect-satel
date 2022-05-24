99e825cc68f289814da155c87fa65142ae6cdbcf
leenperjasknegt <leenknegtjasper@gmail.com>
2022-05-24 21:07:59 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -9,9 +9,9 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 from .constants import PARTITION, ZONE, OUTPUT
 from . import Integra
 
-camecturl = "camect.local"
-camectpassword = "service"
-integrazone = "Poort"
+camecturl = 'camect.local'
+camectpassword = 'service'
+integrazone = 'Poort'
 integrapartition = "Camera's"
 
 template = '''\
